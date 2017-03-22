bbafcc4d1c7e30fb589d8ef07dad318aabf183b7
shiraki.s@opst.co.jp <shiraki.s@opst.co.jp>
2017-03-22 14:58:15 +0900

    独自チェーンコード更新

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -72,3 +72,21 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	return nil, errors.New("Received unknown function query: " + function)
 }
+
+func (t *SimpleChaincode) read(stub * shim.ChaincodeStub, args []string) ([]byte, error) {
+  var key, jsonResp string
+  var err error
+
+  if len(args) != 1 {
+    return nil, errors.New("Incorrenct number of arguments. Expecting name of the key to query")
+  }
+
+  key = args[0]
+  valAsbytes, err := stub.GetState(key)
+  if err != nil {
+    jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+    return nil, errors.New(jsonResp)
+  }
+
+  return valAsbytes, nil
+}
